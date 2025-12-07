2817fb24f64939530ada614ff6e442d62a4de6e2
Yordis Cujar <60682057+yordisc@users.noreply.github.com>
2025-12-07 13:24:43 -0400

    Updates

---
+++ assets/FloatingAdSidebars-BlJFzfze.js
@@ -0,0 +1,1 @@
+import{r as s}from"./index-RhVrZR8b.js";const r=()=>{const[t,e]=s.useState([]);return null};export{r as FloatingAdSidebars};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/link.me/sw.js', { scope: '/link.me/' })})}
