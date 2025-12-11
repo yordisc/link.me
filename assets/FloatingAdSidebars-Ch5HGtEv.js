6d67ad4c91e751b82b55f52a7ea4c2b19b54f562
yordisc <60682057+yordisc@users.noreply.github.com>
2025-12-11 20:05:57 +0000

    Deploying to gh-pages from @ yordisc/link.me-source@3e65fa5bb8abe5956f301cb4236725de17db2494 🚀

---
+++ assets/FloatingAdSidebars-Ch5HGtEv.js
@@ -0,0 +1,1 @@
+import{r as s}from"./index-B8-ixKQf.js";const r=()=>{const[t,e]=s.useState([]);return null};export{r as FloatingAdSidebars};
--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/link.me/sw.js', { scope: '/link.me/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
